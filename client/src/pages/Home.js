33264606931dec282342110d2fb80cfe4526063d
Andres <garelan@gmail.com>
2022-07-06 22:07:54 -0500

    homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,1 +1,37 @@
-.
+import React from "react";
+
+const homePage = () => {
+
+        return (
+            <>
+            <html lang="en">
+
+            <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Material Design for Bootstrap</title>
+
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+
+    <link rel="stylesheet" href="css/mdb.min.css" />
+    </head>
+
+    <body>
+
+
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+
+    <script type="text/javascript"></script>
+    </body>
+
+    </html>
+            </>
+    );
+};
+
+export default homePage
