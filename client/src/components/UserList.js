0f2d34ea2195c8bfdbdc44a460f9e9b69fe69351
Andres <garelan@gmail.com>
2022-07-09 02:00:17 -0500

    userlist structure

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,1 +1,32 @@
-// .
+import { useState } from "react";
+import Modal from "./UserModal";
+
+function UserList() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentUser, setCurrentUser] = useState();
+
+    const [users] = useState([
+        
+    ]);
+const toggleModal = (user, i) => {
+    setCurrentUser({...user, index: i});
+    setIsModalOpen(!isModalOpen);
+}
+
+    return (
+        <div>
+            {isModalOpen && (
+                <Modal onClose={toggleModal} currentUser={currentUser} />
+            )}
+            <div className="flex-row">
+                {currentUser.map((user, i) => (
+                    <ul>
+                        onClick={() => toggleModal(user, i)}
+                        key={user.name}
+                    </ul>
+                ))}
+            </div>
+        </div>
+    );
+};
+export default UserList;
