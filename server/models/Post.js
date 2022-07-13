38215d2adccc52b8d9cf6e8049afa1257115cad4
AD <archied5150@yahoo.com>
2022-07-13 00:01:47 -0500

    replaced dateFormat with moment.js

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 const commentSchema = require('./Comment');
-const dateFormat = require('../utils/dateFormat');
+const moment = require('moment');
 
 const postSchema = new Schema(
   {
@@ -14,8 +14,10 @@ const postSchema = new Schema(
     },
     createdAt: {
       type: Date,
-      default: Date.now,
-      get: (timestamp) => dateFormat(timestamp),
+      default: moment,
+      get: (createdAtVal) => {
+        moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a');
+      },
     },
     username: {
       type: String,
