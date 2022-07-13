875462009af1a2cc3d98aace491457dd35965415
AD <archied5150@yahoo.com>
2022-07-13 00:24:07 -0500

    changed to use public folder instead of build folder

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/public')));
   }
 
   app.get('*', (req, res) => {
