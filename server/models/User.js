c2fae1a6a702ebda3aa1b05f1bbd804ce9349967
AD <archied5150@yahoo.com>
2022-07-18 22:19:52 -0500

    added further email, username, and password validation in the signup form

--- server/models/User.js
+++ server/models/User.js
@@ -20,7 +20,10 @@ const userSchema = new Schema(
       unique: true,
       required: 'Email is required',
       trim: true,
-      match: [/.+\@.+\..+/, 'please fill an valid email address'],
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        'please fill an valid email address',
+      ],
     },
     password: {
       type: String,
